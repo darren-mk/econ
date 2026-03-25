5a480e6084453b95fdcc67ddcd8703b0925235aa
Darren Kim <darren.minsoo.kim@gmail.com>
2026-03-25 10:45:06 -0400

    remove cosmetic settings for org mode

--- mod/languages/_org.el
+++ mod/languages/_org.el
@@ -1,21 +1,3 @@
 ;;; -*- lexical-binding: t; -*-
 
-(use-package org
-  :hook ((org-mode . visual-line-mode)
-         (org-mode . my/org-mode-setup))
-  :config
-  (defun my/org-mode-setup ()
-    "Personal tweaks for Org buffers."
-    (setq-local line-spacing 0.15)
-    (setq org-pretty-entities t
-          org-hide-emphasis-markers t
-          org-image-actual-width '(400))
-    (set-face-attribute 'org-code nil :inherit '(fixed-pitch))
-    (set-face-attribute 'org-verbatim nil :inherit '(fixed-pitch))
-    (set-face-attribute 'org-block nil :inherit '(fixed-pitch))
-    (set-face-attribute 'org-block-begin-line nil :inherit '(fixed-pitch))
-    (set-face-attribute 'org-block-end-line nil :inherit '(fixed-pitch))
-    (set-face-attribute 'org-table nil :inherit '(fixed-pitch))
-    (set-face-attribute 'org-formula nil :inherit '(fixed-pitch))
-    (set-face-attribute 'org-meta-line nil :inherit '(fixed-pitch))
-    (set-face-attribute 'org-document-info-keyword nil :inherit '(fixed-pitch))))
+(use-package org)
