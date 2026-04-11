89cd474736c8fd31c1962ac702bee3c000954337
Darren Kim <darren.minsoo.kim@gmail.com>
2026-04-11 11:53:29 -0400

    remove paredit from elisp minibuffer

--- mod/languages/_el.el
+++ mod/languages/_el.el
@@ -2,5 +2,4 @@
 
 (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
 (add-hook 'ielm-mode-hook #'enable-paredit-mode)
-(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
