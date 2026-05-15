3761225f94a3a971fb5acce5a948e97aaee46ad1
Darren Kim <darren.minsoo.kim@gmail.com>
2026-05-15 08:13:09 -0400

    add keybinding for ignored rust tests

--- mod/languages/_rust.el
+++ mod/languages/_rust.el
@@ -11,4 +11,11 @@
   :custom
   (rustic-lsp-client 'eglot)
   (rustic-format-on-save nil)
-  (rustic-cargo-use-last-stored-arguments t))
+  (rustic-cargo-use-last-stored-arguments t)
+  :config
+  (defun dk/rustic-cargo-test-ignored ()
+    "Run the test at point with --ignored --nocapture flags."
+    (interactive)
+    (compile "cargo test -- --ignored --nocapture"))
+  :bind (:map rustic-mode-map
+              ("C-c C-c C-i" . dk/rustic-cargo-test-ignored)))
